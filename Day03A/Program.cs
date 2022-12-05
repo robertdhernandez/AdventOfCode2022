332f89389a48cb9a110f888560cf82f9aafb464e
Robert Hernandez <hernandez.robert.d@outlook.com>
2022-12-04 21:51:22 -0500

    Add solution for Day03

---
+++ Day03A/Program.cs
@@ -0,0 +1,15 @@
+﻿static (string, string) Split(string input) => (input.Substring(0, input.Length / 2), input.Substring(input.Length / 2));
+static int GetPriority(char c) => char.ToLower(c) - 'a' + 1 + (26 * Convert.ToInt32(char.IsUpper(c)));
+
+static IEnumerable<int> ReadInput(TextReader reader)
+{
+    string? line;
+    while (!string.IsNullOrEmpty(line = reader.ReadLine()))
+    {
+        (string a, string b) = Split(line);
+        yield return a.Intersect(b).Sum(GetPriority);
+    }
+}
+
+int result = ReadInput(Console.In).Sum();
+Console.WriteLine(result);
---
+++ Day03B/Program.cs
@@ -0,0 +1,16 @@
+﻿static int GetPriority(char c) => char.ToLower(c) - 'a' + 1 + (26 * Convert.ToInt32(char.IsUpper(c)));
+
+static IEnumerable<char> ReadInput(TextReader reader)
+{
+    string? line;
+    while (!string.IsNullOrEmpty(line = reader.ReadLine()))
+    {
+        yield return line
+            .Intersect(reader.ReadLine()!)
+            .Intersect(reader.ReadLine()!)
+            .Single();
+    }
+}
+
+int result = ReadInput(Console.In).Sum(GetPriority);
+Console.WriteLine(result);
